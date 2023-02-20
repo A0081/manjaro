c3a1026211f3117eab54b3c83b614cad5b1f2945
_0081 <S_0081@163.com>
2023-02-20 18:20:26 +0800

    2

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,9 +1,20 @@
-export default {
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+//------------------------------全局配置------------------------------------------
+    title: 'Manjaro',
+    description: 'A Manjaro Guide site',
+    lastUpdated: true ,//最后更新时间
+    titleTemplate: ' _0081', //该选项为title | _0081
+    cleanUrls: true,
+
+
+//-------------------------------主题配置-----------------------------------------
     themeConfig: {
       siteTitle: 'Manjaro',
       logo: '/manjaro.png',
       socialLinks: [
-        { icon: 'github', link: 'https://github.com/A0081' },
+        { icon: 'github', link: 'https://github.com/A0081' },//  这里要改为项目的github连接
         // You can also add custom icons by passing SVG as string:
         {
           icon: {
@@ -11,6 +22,11 @@ export default {
           },
           link: 'https://www.bilibili.com'
         }
-      ]
-    }
-  }
+      ],
+      editLink: {
+        pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+        text: 'Edit this page on GitHub'
+      },
+    },
+    
+  })
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,38 @@
-# Hello VitePress
+---
+layout: home
+
+hero:
+  name: Manjaro 
+  text: 人生苦短我用Manjaro.
+  tagline: 让Manjaro成为你的常用系统把！
+  image:
+    src: /logo.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text: 🍺Get Started
+      link: /1
+    - theme: alt
+      text: 🍌 GitHub
+      link: https://github.com/vuejs/vitepress
+
+features:
+  - icon: 📖
+    title: 格式优化
+    details: 本文为第三版，由Vitepress 驱动，进行了格式优化
+  - icon: 🎏
+    title: 介绍详细
+    details: 细节和原理进行了更为详细的说明，知其然知其所以然
+  - icon: 🏝️
+    title: 排版优化
+    details: 去除了大量操作演示类图片，以精炼的语言提供教学
+  - icon: ❎
+    title: 科学上网
+    details: 为网络封锁区域提供了简易的科学上网流程
+  - icon: ✅
+    title: 实用至上
+    details: 包含了大量常用软件，对非自由软件更加包容
+  - icon: 🌱
+    title: 参与贡献
+    details: 目前指南有许多需要改进的地方，参阅贡献章节帮助完善
+---
